d5e8f0ed77534b2d1983a47a2e877c76501a9117
nonaana111192-max <nonaana111192@gmail.com>
2025-11-13 16:00:26 +0700

    Update StageOne.js

--- app/components/StageOne.js
+++ app/components/StageOne.js
@@ -11,16 +11,16 @@ export default function StageOne({ next }) {
     >
       <h1 className="text-3xl font-semibold mb-3">Halo!</h1>
       <p className="text-gray-600 leading-relaxed">
-        Hai jelek, istirahat cukup, makan cukup, jangan 
-        overthinking nanti naik aslam nya
-          hehehe
+       Lihat lah Titi ini ya,
+        bisa-bisanya di depan mata malah memperhatikan pria lain
+        huffftttt...
       </p>
 
       <button
         onClick={next}
         className="mt-6 px-6 py-3 bg-pink-500 text-white rounded-xl text-lg shadow-sm active:scale-95"
       >
-        Lanjutkan
+        Lihat kelanjutannya
       </button>
     </motion.div>
   );
