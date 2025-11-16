6908f1069b0639dacc2443b3e9db9e52c0cdc299
nonaana111192-max <nonaana111192@gmail.com>
2025-11-16 18:57:36 +0700

    Update StageTwo.js

--- app/components/StageTwo.js
+++ app/components/StageTwo.js
@@ -14,7 +14,7 @@ export default function StageTwo({ next, back }) {
       transition={{ duration: 0.4 }}
       className="w-full max-w-md mx-4 p-6 rounded-2xl bg-white shadow-md"
     >
-      <h2 className="text-2xl font-semibold mb-3">Sekarang harus gimana?</h2>
+      <h2 className="text-2xl font-semibold mb-3">Pesannya mamas</h2>
       <h1 className="text-gray-700 space-y-2 text-left">
        Apapun yang sedang kamu lakukan, kamu usahakan, selalu minta petunjuk sama Allah ya
       </h1>
