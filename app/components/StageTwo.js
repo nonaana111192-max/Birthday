63c564ed2c58f38123c3c5f3524f751dd39e99a6
nonaana111192-max <nonaana111192@gmail.com>
2025-11-26 13:27:10 +0700

    Update StageTwo.js

--- app/components/StageTwo.js
+++ app/components/StageTwo.js
@@ -14,9 +14,13 @@ export default function StageTwo({ next, back }) {
       transition={{ duration: 0.4 }}
       className="w-full max-w-md mx-4 p-6 rounded-2xl bg-white shadow-md"
     >
-      <h2 className="text-2xl font-semibold mb-3">Pesannya mamas</h2>
+      <h2 className="text-2xl font-semibold mb-3">kata-kata dari mamas</h2>
       <h1 className="text-gray-700 space-y-2 text-left">
-       Apapun yang sedang kamu lakukan, kamu usahakan, selalu minta petunjuk sama Allah ya
+       Andai kita diberikan kuasa untuk memutar waktu agar bisa menjalani apa yang kita inginkan, kita usahakan, 
+       kamu pasti akan tau betapa besar dan luas nya rasa yang kuberi padamu.
+      Tentu tak akan kubiarkan kesedihan bersembunyi di relung hatimu yang rapuh,
+        akan terus aku upayakan rasa tenang, senang, dan nyaman itu kokoh menguasai seluruh isi hatimu,
+        jiwa dan raga
       </h1>
 
       <div className="mt-6 flex justify-between">
