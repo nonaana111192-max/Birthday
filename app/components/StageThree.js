5dc28001396daf3849ba2d9d9a4bdd3e98b75e17
nonaana111192-max <nonaana111192@gmail.com>
2025-12-02 22:33:09 +0700

    Update StageThree.js

--- app/components/StageThree.js
+++ app/components/StageThree.js
@@ -2,7 +2,8 @@
 import { motion } from "framer-motion";
 
 export default function StageThree({ next, back }) {
-  const message = `Lihat betapa bahagianya dia`;
+  const message = `Bahkan ribuan benda angkasa yang membentuk astrologi tak kuasa menahan dirinya untuk menyatakan kesempurnaan 
+  kita yang seharusnya`;
 
   return (
     <motion.div
@@ -11,11 +12,11 @@ export default function StageThree({ next, back }) {
       transition={{ duration: 0.4 }}
       className="w-full max-w-md mx-4 p-6 rounded-2xl bg-white shadow-md text-center"
     >
-      <h2 className="text-2xl font-semibold mb-3">Kayak si Nailong</h2>
+      <h2 className="text-2xl font-semibold mb-3">Astrologi</h2>
 
       <img
-        src="https://media1.tenor.com/m/aqQM4YJbKJUAAAAC/nailong.gif"
-        alt="Nailong"
+        src="https://astrotalk.com/astrology-blog/wp-content/uploads/2023/09/p-and-s-1-1.jpg"
+        alt="Scorpisces"
         className="mx-auto mb-4 rounded-lg"
       />
 
